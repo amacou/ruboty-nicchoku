a22045b9e2d607ec53db5a632418b506a10738f5
amacou <amano@pepabo.com>
2015-02-04 09:35:25 +0900

    test

--- lib/ruboty/handlers/nicchoku.rb
+++ lib/ruboty/handlers/nicchoku.rb
@@ -4,7 +4,7 @@ module Ruboty
       NAMESPACE = "nicchoku"
       MINE_REGEXP = "わたく?し|ワタク?シ|あたし|アタシ|私|ボク|ぼく|僕|オレ|おれ|俺|ちん|チン|朕|じぶん|自分|"
       on(
-        /([a-zA-Z:_\- ]+)?(#{MINE_REGEXP})が日直です|日直は(#{MINE_REGEXP})です/,
+        /([a-zA-Z_\- ]+)?(#{MINE_REGEXP})が日直です|日直は(#{MINE_REGEXP})です/,
         description: "言った人が日直になります",
         name: "stand_up",
         all: true
